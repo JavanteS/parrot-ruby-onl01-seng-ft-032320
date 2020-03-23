64a3e06551c7867387e21407bd8766701229b5ae
Javante Nelson <javantenelson@gmail.com>
2020-03-23 19:57:24 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot()
- puts "Squawk!"
- "Squawk!"
+def parrot(name="Squawk!")
+ puts name
+ return name
 end
 
 
